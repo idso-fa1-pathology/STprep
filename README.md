b230f8f5b589f4de98110b395ec307653069edc9
xi11 <xiaoxipan3@gmail.com>
2024-09-08 22:37:35 -0500

    update

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Spatial transcriptomics/10x visium data preparation
 This repository aims to detail the processing of space ranger pipeline from FATSTQ files to gene expression matrix.
 
-## Download spaceranger
+## T6 - Download spaceranger
 `curl -o spaceranger-3.0.1.tar.gz "https://cf.10xgenomics.com/releases/spatial-exp/spaceranger-3.0.1.tar.gz?Expires=1724480948&Key-Pair-Id=APKAI7S6A5RYOXBWRPDA&Signature=HVB5cU5s~le6QS8S9EQ2SUvHVcA9gfjTMzY70dyQpK2PTP14pvlwIke2T8lWbtnyuRJRegeameYZ~C80p6NNl3mH7VF1dQh6SY6HeBOfqcXbtaIOErNaPeqzfStiRJvtHIGDTNHgMbuO5qT6VBZgEqoCNQkuLSx3VS~i2kNoBd0BT5HXdKaeHS4BzcdiPtP2fNusG0esOzNY16WnASLkx4RB-U3KeouHOd8T8Ry9RlTwdNAJev2m3ZGoCmW2HiXoenQJ9tafmhAwkd9vltkETXSXfXf1FVi0kKvi84yOrmzebngTylJeuX7L~o6bGIOx1Mb~pkElN0trwcJx7jCqNA__"`
 
-## Install spaceranger (decompression)
-- `tar -xzvf spaceranger-3.0.1.tar.gz`
+## T6 - Install spaceranger
+- Unpack, `tar -xzvf spaceranger-3.0.1.tar.gz`
 
 - Prepend the Space Ranger directory to your $PATH. This allows you to invoke the spaceranger command.
 `$ export PATH=/home/xpan7/spaceranger-3.0.1:$PATH`
@@ -18,6 +18,19 @@ After editing, save the changes and exit the editor by pressing Ctrl + X, then Y
 
 - To test if spaceranger is installed successfully
 
-navigate to your own dir not yuanlab at least, `/rsrch5/home/trans_mol_path/xpan7/project`
+navigate to your own dir, `/rsrch5/home/trans_mol_path/xpan7/project`, to ensure the output of testing sample is stored in your own dir instead of yuanlab dir.
 
 then `spaceranger testrun --id=tiny`
+
+
+## Seadragon
+There is spaceranger module installed in Seadragon, please refer to `sample_specific_spaceranger_hpc.lsf` to run spaceranger in Seadragon.
+
+Note that,
+
+- You'll see the following after `module load spaceranger`, but it doesn't mean the spaceranger version is 1.1.0; it's actually 3.0.1 if using `spaceranger --version` to check
+
+`Reference data are located in path
+                /rsrch3/scratch/reflib/REFLIB_data/spaceranger-1.1.0`
+
+- The error logs can be found in .out files. If you cannot find the error info in .err files, also check with .out files.
--- sample_specific_spaceranger_T6.sh
+++ sample_specific_spaceranger_T6.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+# final version
 export PATH=/home/xpan7/spaceranger-3.0.1:$PATH
 cd /rsrch6/home/trans_mol_path/yuan_lab/TIER2/anthracosis/visium_TMA5primary2014/spatial-transcriptome/
 
-spaceranger count --id=TMA5-015_manual_fiducial_hpc_test \
+spaceranger count --id=TMA5-015_manual_fiducial_T6v2 \
     --transcriptome=/rsrch6/home/trans_mol_path/yuan_lab/TIER2/anthracosis/visium_TMA5primary2014/ref-data/refdata-gex-GRCh38-2020-A \
     --probe-set=/rsrch6/home/trans_mol_path/yuan_lab/TIER2/anthracosis/visium_TMA5primary2014/ref-data/Visium_Human_Transcriptome_Probe_Set_v2.0_GRCh38-2020-A.csv \
     --fastqs=/rsrch6/home/trans_mol_path/yuan_lab/TIER1/anthracosis/visium_TMA5primary_2014-/seq_data \
@@ -14,7 +15,55 @@ spaceranger count --id=TMA5-015_manual_fiducial_hpc_test \
     --area=A1 \
     --slidefile=/rsrch6/home/trans_mol_path/yuan_lab/TIER2/anthracosis/visium_TMA5primary2014/slidefile/V53B02-382.gpr \
     --loupe-alignment=/rsrch6/home/trans_mol_path/yuan_lab/TIER2/anthracosis/visium_TMA5primary2014/fiducial_aligment_json/V53B02-382-A1_015.json \
+    --localcores=8 \
+    --localmem=64
+
+# 157
+spaceranger count --id=TMA5-157_auto_T6v3 \
+    --transcriptome=/rsrch6/home/trans_mol_path/yuan_lab/TIER2/anthracosis/visium_TMA5primary2014/ref-data/refdata-gex-GRCh38-2020-A \
+    --probe-set=/rsrch6/home/trans_mol_path/yuan_lab/TIER2/anthracosis/visium_TMA5primary2014/ref-data/Visium_Human_Transcriptome_Probe_Set_v2.0_GRCh38-2020-A.csv \
+    --fastqs=/rsrch6/home/trans_mol_path/yuan_lab/TIER1/anthracosis/visium_TMA5primary_2014-/seq_data \
+    --sample=LUADC-157 \
+    --cytaimage=/rsrch6/home/trans_mol_path/yuan_lab/TIER1/anthracosis/visium_TMA5primary_2014-/VisiumCytAssistRunInfo/assay_CAVG10434_LUADC_V53B02-382_1722442225_CytAssist/CAVG10434_2024-07-31_11-10-25_LUADC_V53B02-382_B_157.tif \
+    --image=/rsrch6/home/trans_mol_path/yuan_lab/TIER1/anthracosis/visium_TMA5primary_2014-/HE40x_tif/TMA5-157-svs.tif \
+    --slide=V53B02-382 \
+    --area=B \
+    --slidefile=/rsrch6/home/trans_mol_path/yuan_lab/TIER2/anthracosis/visium_TMA5primary2014/slidefile/V53B02-382.gpr \
     --override-id \
+    --create-bam=true \
+    --localcores=16 \
+    --localmem=128
+
+
+# 142
+spaceranger count --id=TMA5-142_manual_fiducial_T6v3 \
+    --transcriptome=/rsrch6/home/trans_mol_path/yuan_lab/TIER2/anthracosis/visium_TMA5primary2014/ref-data/refdata-gex-GRCh38-2020-A \
+    --probe-set=/rsrch6/home/trans_mol_path/yuan_lab/TIER2/anthracosis/visium_TMA5primary2014/ref-data/Visium_Human_Transcriptome_Probe_Set_v2.0_GRCh38-2020-A.csv \
+    --fastqs=/rsrch6/home/trans_mol_path/yuan_lab/TIER1/anthracosis/visium_TMA5primary_2014-/seq_data \
+    --sample=LUADC-142 \
+    --cytaimage=/rsrch6/home/trans_mol_path/yuan_lab/TIER1/anthracosis/visium_TMA5primary_2014-/VisiumCytAssistRunInfo/assay_CAVG10434_LUADC_V53B02-066_1721838368_CytAssist/CAVG10434_2024-07-24_11-26-08_LUADC_V53B02-066_A_142.tif \
+    --image=/rsrch6/home/trans_mol_path/yuan_lab/TIER1/anthracosis/visium_TMA5primary_2014-/HE40x_tif/TMA5-142-svs.tif \
+    --slide=V53B02-066 \
+    --area=A1 \
+    --slidefile=/rsrch6/home/trans_mol_path/yuan_lab/TIER2/anthracosis/visium_TMA5primary2014/slidefile/V53B02-066.gpr \
+    --loupe-alignment=/rsrch6/home/trans_mol_path/yuan_lab/TIER2/anthracosis/visium_TMA5primary2014/fiducial_aligment_json/V53B02-066-A1_142.json \
+    --create-bam=true \
+    --localcores=16 \
+    --localmem=128
+
+
+# 161
+spaceranger count --id=TMA5-161_manual_fiducial_T6v3 \
+    --transcriptome=/rsrch6/home/trans_mol_path/yuan_lab/TIER2/anthracosis/visium_TMA5primary2014/ref-data/refdata-gex-GRCh38-2020-A \
+    --probe-set=/rsrch6/home/trans_mol_path/yuan_lab/TIER2/anthracosis/visium_TMA5primary2014/ref-data/Visium_Human_Transcriptome_Probe_Set_v2.0_GRCh38-2020-A.csv \
+    --fastqs=/rsrch6/home/trans_mol_path/yuan_lab/TIER1/anthracosis/visium_TMA5primary_2014-/seq_data \
+    --sample=LUADC-161 \
+    --cytaimage=/rsrch6/home/trans_mol_path/yuan_lab/TIER1/anthracosis/visium_TMA5primary_2014-/VisiumCytAssistRunInfo/assay_CAVG10434_LUADC_V53B02-066_1721838368_CytAssist/CAVG10434_2024-07-24_11-26-08_LUADC_V53B02-066_B_161.tif \
+    --image=/rsrch6/home/trans_mol_path/yuan_lab/TIER1/anthracosis/visium_TMA5primary_2014-/HE40x_tif/TMA5-161-svs.tif \
+    --slide=V53B02-066 \
+    --area=B1 \
+    --slidefile=/rsrch6/home/trans_mol_path/yuan_lab/TIER2/anthracosis/visium_TMA5primary2014/slidefile/V53B02-066.gpr \
+    --loupe-alignment=/rsrch6/home/trans_mol_path/yuan_lab/TIER2/anthracosis/visium_TMA5primary2014/fiducial_aligment_json/V53B02-066-B1_161.json \
     --create-bam=true \
     --localcores=16 \
     --localmem=128
